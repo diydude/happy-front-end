6108e56be2ab1eb5615c0476c0316c0447230469
DIY Dude <artwaveshow@gmail.com>
2023-03-10 17:02:59 +0400

    arrays homework

--- js-intro/homework3/arrays.js
+++ js-intro/homework3/arrays.js
@@ -9,7 +9,15 @@
 
 // P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
 
-
+function camelize(str) {
+	let arr = str.split('-'); // разбивает строку на массив ['string', 'for', 'example']
+	let capArr = arr.map((word, index) => { // капитализует
+		if (index === 0) return word // исключая первый элемент
+		else return word.charAt(0).toUpperCase() + word.slice(1);
+	});
+	return capArr.join(''); // склеивает без пробелов и запятых
+}
+console.log(camelize('string-for-example'));
 
 // 2)
 // Задача: написать функцию, принимающую массив чисел
@@ -22,4 +30,19 @@
 
 // Сохраняйте вызов этой функции через деструктурирующее присваивание
 
+let sumOfPositiveAndNegativeNumbers = (array) => {
+	let positiveNumbers = array.reduce((sum, num) => {
+		if (num > 0) sum += num;
+		return sum;
+	}, 0);
+	let negativeNumbers = array.reduce((sum, num) => {
+		if (num < 0) sum += num;
+		return sum;
+	}, 0);
+	let sumArray = [positiveNumbers, negativeNumbers];
+	return sumArray;
+}
 
+const arr = [1, -2, 3, 4, -9];
+const [sumOfPositive, sumOfNegative] = sumOfPositiveAndNegativeNumbers(arr);
+console.log(sumOfPositive, sumOfNegative);
